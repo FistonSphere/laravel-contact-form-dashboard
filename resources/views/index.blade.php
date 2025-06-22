52e630999484d53c7312d17dbaa445aa1a207878
fistonsano04 <sanofiston04@gmail.com>
2025-06-22 21:51:40 +0200

    designing index blade page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="content">
+                <h3>Welcome on Form contacting page.</h3>
+            </div>
+            <div class="btn">
+                <div class="login">
+                    <button>Login</button>
+                </div>
+                <div class="register">
+                    <button>Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>

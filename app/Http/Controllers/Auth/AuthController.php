057ff120eed2dcdff586e9e248f3889d16289c59
fistonsano04 <sanofiston04@gmail.com>
2025-06-23 17:57:35 +0200

    login functionality

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,5 +7,25 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $request->validate(
+            [
+                'email' => 'required|email|exists:users,email',
+                'password' => 'required|min:6',
+            ],
+            [
+                'email.required' => 'Email is required',
+                'password.required' => 'Password is required',
+                'email.exists' => 'Email does not exist in our records',
+                'password.min' => 'Password must be at least 6 characters',
+            ]
+        );
+
+        if (auth()->attempt($request->only('email', 'password'))) {
+            return redirect()->route('dashboard')->with('success', 'Login successful');
+        } else {
+            return redirect()->back()->withErrors(['email' => 'Invalid credentials'])->withInput();
+        }
+    }
 }

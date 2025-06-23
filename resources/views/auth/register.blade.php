01200fb530bdda22b519ac37194245474b858c49
fistonsano04 <sanofiston04@gmail.com>
2025-06-23 08:42:47 +0200

    updating register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,12 +15,21 @@
         <form action="{{ route('register.post') }}">
             @csrf
             <div class="form-group">
+                <label for="name">Full Name:</label>
+                <input type="text" name="fname" id="fname" class="form-control" required>
+            </div>
+            <div class="form-group">
                 <label for="email">Email:</label>
                 <input type="email" name="email" id="email" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
                 <input type="password" name="password" id="password" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password_confirmation">Confirm Password:</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"
+                    required>
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary">Login</button>

9a63751b8483abb7d85d0361490a46f2fc537e8f
fistonsano04 <sanofiston04@gmail.com>
2025-06-23 18:04:23 +0200

    register user functionality

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -27,5 +28,29 @@ class AuthController extends Controller
         } else {
             return redirect()->back()->withErrors(['email' => 'Invalid credentials'])->withInput();
         }
+    }
+
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'fname' => 'required|string|max:255',
+            'password' => 'required|string|min:6|confirmed',
+            'email' => 'required|email|unique:users,email'
+        ], [
+            'fname.required' => 'First name is required',
+            'password.required' => 'Password is required',
+            'email.required' => 'Email is required',
+            'email.unique' => 'Email already exists',
+            'password.min' => 'Password must be at least 6 characters',
+            'password.confirmed' => 'Passwords do not match',
+        ]);
+
+        $user = new User();
+        $user->fname = $request->input('fname');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+        return redirect()->route('login')->with('success', 'Registration successful, please login');
     }
 }

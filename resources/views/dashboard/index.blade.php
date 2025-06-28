2300f7a9651d4da5def8d2bde80e76934f866c0e
fistonsano04 <sanofiston04@gmail.com>
2025-06-28 23:21:46 +0200

    initial commit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,8 @@
 
     <div class="container">
-        <h1>Welcome to the Dashboard</h1>
-        <p>You are logged in as {{ auth()->user()->fname }}</p>
-        <a href="{{ route('logout') }}" class="btn btn-danger">Logout</a>
+  <div class="contact-form">
+    
+  </div>
     </div>
+
+

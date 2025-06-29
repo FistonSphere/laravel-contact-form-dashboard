fbc0e463fa6c489bf2a837ca21c61b3a910a818d
fistonsano04 <sanofiston04@gmail.com>
2025-06-29 22:58:08 +0200

    initial commit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,19 @@
 <div class="container">
     <div class="contact-form">
-<form action="" method="post">
-    @csrf
-    <div class="form-group">
-        
-    </div>
-</form>
+        <form action="" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="username">Full Name:</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea name="" id=""></textarea>
+            </div>
+        </form>
     </div>
 </div>

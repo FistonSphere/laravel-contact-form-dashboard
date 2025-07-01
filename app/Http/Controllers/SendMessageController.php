ecf9f3aa380a91141698224cc94ed39e11b9ab07
fistonsano04 <sanofiston04@gmail.com>
2025-07-01 17:49:35 +0200

    fixing redirects messages|validations

--- app/Http/Controllers/SendMessageController.php
+++ app/Http/Controllers/SendMessageController.php
@@ -12,19 +12,25 @@ class SendMessageController extends Controller
     {
         dd($request->all());
         $request->validate([
-            'name'=>'required|string|max:255',
-            'email'=>'required|email|max:255',
-            'subject'=>'required|string|max:255',
-            'message'=>'required|string|max:5000',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string|max:5000',
         ]);
 
         //send message logic here via gmail or any other service
-        $name = $request->input('name');
-        $email = $request->input('email');
-        $subject = $request->input('subject');
-        $message = $request->input('message');
+        if ($request->has('name') && $request->has('email') && $request->has('subject') && $request->has('message')) {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $subject = $request->input('subject');
+            $message = $request->input('message');
 
-        //Mail facade functionalitu can be used here
-        Mail::to($email)->send(new SendMessage($name, $email, $subject, $message));
+            //Mail facade functionalitu can be used here
+            Mail::to($email)->send(new SendMessage($name, $email, $subject, $message));
+
+            return redirect()->back()->with('success', 'Message sent successfully');
+        } else {
+            return redirect()->back()->with('error', 'All fields are required');
+        }
     }
 }

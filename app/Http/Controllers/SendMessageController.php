bf7f942864582d872d8a76162ba5026addcaf177
fistonsano04 <sanofiston04@gmail.com>
2025-07-01 20:54:58 +0200

    assigning message to be saved into database

--- app/Http/Controllers/SendMessageController.php
+++ app/Http/Controllers/SendMessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\SendMessage;
+use App\Models\MailMessage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -23,11 +24,18 @@ class SendMessageController extends Controller
             $email = $request->input('email');
             $customSubject = $request->input('subject');
             $Usermessage = $request->input('message');
+            //saving message to database
 
+            $saveMessage = new MailMessage();
+            $saveMessage->name = $name;
+            $saveMessage->email = $email;
+            $saveMessage->customSubject = $customSubject;
+            $saveMessage->message = $Usermessage;
+            $saveMessage->save();
             //Mail facade functionalitu can be used here
             Mail::to($email)->send(new SendMessage($name, $email, $customSubject, $Usermessage));
 
-            return redirect()->back()->with('success', 'Message sent successfully');
+            return redirect()->back()->with('success', 'Message sent successfully and saved successfully 😊');
         } else {
             return redirect()->back()->with('error', 'All fields are required');
         }

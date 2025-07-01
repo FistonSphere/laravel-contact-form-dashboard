cc62c1302ad7612a366b1ad8df00a0aa407cda77
fistonsano04 <sanofiston04@gmail.com>
2025-07-01 15:45:56 +0200

    mail sending functionality

--- app/Http/Controllers/SendMessageController.php
+++ app/Http/Controllers/SendMessageController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class SendMessageController extends Controller
 {
-    //
+    public function send(Request $request)
+    {
+        dd($request->all());
+        $request->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|email|max:255',
+            'subject'=>'required|string|max:255',
+            'message'=>'required|string|max:5000',
+        ]);
+
+        //send message logic here via gmail or any other service
+    }
 }

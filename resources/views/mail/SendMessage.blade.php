a9572f510518766dbb3f17c5035a971f8aa53c28
fistonsano04 <sanofiston04@gmail.com>
2025-07-01 17:45:28 +0200

    mail template design

---
+++ resources/views/mail/SendMessage.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Incoming Message</title>
+</head>
+<body>
+<div class="container">
+    <table>
+        <th>
+            <tr>Name</tr>
+            <tr>Email</tr>
+            <tr>Message</tr>
+        </th>
+        <tbody>
+            <tr>
+                <td>{{ $name }}</td>
+                <td>{{ $email }}</td>
+                <td>{{ $message }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
